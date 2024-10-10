e389732a3b9e87d432eb33a20b7fd15695100b6e
BaimukhametovaMD <Tyuiu.BaimuhametovaMD@gmail.com>
2024-10-10 18:02:43 +0500

    task6

--- Tyuiu.BaimuhametovaMD.Sprint3.Task6.V18.Test/DataServiceTest.cs
+++ Tyuiu.BaimuhametovaMD.Sprint3.Task6.V18.Test/DataServiceTest.cs
@@ -10,6 +10,7 @@ namespace Tyuiu.BaimuhametovaMD.Sprint3.Task6.V18.Test
         {
             DataService ds = new DataService();
             int start = 9, end = 18;
+            Assert.AreEqual(2, ds.GetSumTheDivisors(start, end));
         }
     }
 }
--- Tyuiu.BaimuhametovaMD.Sprint3.Task6.V18/Program.cs
+++ Tyuiu.BaimuhametovaMD.Sprint3.Task6.V18/Program.cs
@@ -6,7 +6,26 @@ namespace Tyuiu.BaimuhametovaMD.Sprint3.Task6.V18
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+            int startValue = 9, endValue = 18;
+            Console.Title = "Спринт #3 | Выполнил: Баймухаметова М.Д. | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Обработка целочисленной информации                                *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Баймухаметова Мадина | ИБКСб-24-1                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Напишите программу, которая ищет среди целых чисел,                     *");
+            Console.WriteLine("* принадлежащих числовому отрезку [9, 18]                                 *");
+            Console.WriteLine("* сумму всех делителей меньше 10                                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Сумма делителей равна " + ds.GetSumTheDivisors(startValue, endValue));
+            Console.ReadLine();
         }
     }
 }
